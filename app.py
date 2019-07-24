0c4f6bc217819e6759d8bb7f8fc97f7fe1f74bc7
Schneider Raphaël <rschneid@L102501.igfl.ens-lyon.fr>
2019-07-24 15:31:35 +0200

    add options

--- app.py
+++ app.py
@@ -23,22 +23,51 @@ app.layout = html.Div(children=[
 
         html.Div(children="Merge two files based on column identity")
     ], style = {"textAlign":"center"}),
-    html.Hr(),
 
 	html.Div(children=[
-        html.Label('Select if a header is present in your files or not'),
-
-        dcc.RadioItems(
-            id='id_switch_header',
-            options=[
-                {'label': 'Header', 'value': "Header"},
-                {'label': 'No Header', 'value': "No_Header"},
-            ],
-            value="Header",
-            labelStyle={'display': 'inline-block'}
-        ), 
         html.Hr(),
-        html.Label('Select first file'),
+        html.H3("First file information"),
+        html.Div(className="row",children=[
+			html.Div(className="five columns",children=[
+		        html.Label('Select file type'),
+
+		        dcc.RadioItems(
+		            id='id_radio_fileType1',
+		            options=[
+		                {'label': 'tsv', 'value': "\t"},
+		                {'label': 'csv', 'value': ","},
+		                #{'label': 'excel', 'value': "excel"}
+		            ],
+		            value="\t",
+		            labelStyle={'display': 'inline-block'}
+		            
+		        ), 
+		    ], style = {"textAlign":"right"}),
+		    html.Div(className="two columns",children=[
+		        html.Label('Header present in file?'),
+
+		        dcc.RadioItems(
+		            id='id_switch_header1',
+		            options=[
+		                {'label': 'Header', 'value': "Header"},
+		                {'label': 'No Header', 'value': "No_Header"},
+		            ],
+		            value="Header",
+		            labelStyle={'display': 'inline-block'}
+		        ), 
+		    ], style = {"textAlign":"center"}),
+		    html.Div(className="five columns",children=[
+		        html.Label('Enter caracters until wich you want to keep the name'),
+		        html.Label('For example, if you have NM_123456.1. You can remove the version of the gene by entering "."'),
+
+		        dcc.Input(
+		            id='id_input_split1',
+		            placeholder="Caracters to split",
+		            value=None
+		        ), 
+		    ], style = {"textAlign":"left"}),
+		]),
+        html.H5('Select first file'),
         dcc.Upload(
             id="upload-f1",
             children=html.Div(['Drag and Drop or ', html.A('Select File')]),
@@ -59,7 +88,49 @@ app.layout = html.Div(children=[
 
     html.Hr(),
     html.Div(children=[
-        html.Label('Select second file'),
+    	html.H3("Second file information"),
+        html.Div(className="row",children=[
+			html.Div(className="five columns",children=[
+		        html.Label('Select file type'),
+
+		        dcc.RadioItems(
+		            id='id_radio_fileType2',
+		            options=[
+		                {'label': 'tsv', 'value': "\t"},
+		                {'label': 'csv', 'value': ","},
+		                #{'label': 'excel', 'value': "excel"}
+		            ],
+		            value="\t",
+		            labelStyle={'display': 'inline-block'}
+		            
+		        ), 
+		    ], style = {"textAlign":"right"}),
+		    html.Div(className="two columns",children=[
+		        html.Label('Header present in file?'),
+
+		        dcc.RadioItems(
+		            id='id_switch_header2',
+		            options=[
+		                {'label': 'Header', 'value': "Header"},
+		                {'label': 'No Header', 'value': "No_Header"},
+		            ],
+		            value="Header",
+		            labelStyle={'display': 'inline-block'}
+		        ), 
+		    ], style = {"textAlign":"center"}),
+		    
+		    html.Div(className="five columns",children=[
+		        html.Label('Enter caracters until wich you want to keep the name'),
+		        html.Label('For example, if you have NM_123456.1. You can remove the version of the gene by entering "."'),
+
+		        dcc.Input(
+		            id='id_input_split2',
+		            placeholder="Caracters to split",
+		            value=None
+		        ), 
+		    ], style = {"textAlign":"left"}),
+		]),
+        html.H5('Select second file'),
         dcc.Upload(
             id="upload-f2",
             children=html.Div(['Drag and Drop or ', html.A('Select File')]),
@@ -92,14 +163,15 @@ app.layout = html.Div(children=[
             [Output('upload-f1-res',"children"),
             Output('upload-f1',"children"),
             Output('msg-select1',"children")],
-            [Input('upload-f1',"contents")],
+            [Input('upload-f1',"contents"),
+            Input('id_radio_fileType1','value')],
             [State('upload-f1',"filename")])
-def read_header1(contents,filename):
+def read_header1(contents,filetype,filename):
     ''' update for first input file '''
     if contents is not None:
         msg_select = "Select the column to synchronize"
         newName = filename
-        list_header_tab = treatFile(contents)
+        list_header_tab = treatFile(contents,filetype)
         return list_header_tab, newName, msg_select
     else:
         newName = html.Div(['Drag and Drop or ', html.A('Select File')])
@@ -109,14 +181,15 @@ def read_header1(contents,filename):
             [Output('upload-f2-res',"children"),
             Output('upload-f2',"children"),
             Output('msg-select2',"children")],
-            [Input('upload-f2',"contents")],
+            [Input('upload-f2',"contents"),
+            Input('id_radio_fileType2','value')],
             [State('upload-f2',"filename")])
-def read_header2(contents,filename):
+def read_header2(contents,filetype,filename):
     ''' update for second input file '''
     if contents is not None:
         msg_select = "Select the column to synchronize"
         newName = filename
-        list_header_tab = treatFile(contents)
+        list_header_tab = treatFile(contents,filetype)
         return list_header_tab, newName, msg_select
     else:
         newName = html.Div(['Drag and Drop or ', html.A('Select File')])
@@ -126,25 +199,34 @@ def read_header2(contents,filename):
             [Output("download_common","children"),
             Output("download_unique","children")],
             [Input('button',"n_clicks")],
-            [State('id_switch_header',"value"),
-            State('upload-f1',"filename"),
+            [State('upload-f1',"filename"),
             State('upload-f1',"contents"), 
             State('upload-f1-res',"value"),
+            State('id_radio_fileType1','value'),
+            State('id_switch_header1',"value"),
+            State('id_input_split1',"value"),
             State('upload-f2',"filename"),
             State('upload-f2',"contents"),
-            State('upload-f2-res',"value")])
-def getRes(n_clicks,header_val, filename1,content1,col1, filename2,content2,col2):
+            State('upload-f2-res',"value"),
+            State('id_radio_fileType2','value'),
+            State('id_switch_header2',"value"),
+            State('id_input_split2',"value")])
+def getRes(n_clicks, filename1,content1,col1,filetype1,header_val1,splitCar1, filename2,content2,col2,filetype2,header_val2,splitCar2):
     if col1 is not None and col2 is not None:
-        
-        if header_val == "Header":
-            header = False
+        #check if header is present for both files
+        if header_val1 == "Header":
+            header1 = False
         else:
-            header = True
-        print(header_val, header)
+            header1 = True
+        print(header_val1, header1)
+        if header_val2 == "Header":
+            header2 = False
+        else:
+            header2 = True
         #print n_clicks, header, col1, col2
-        dico1, h1 = createDicoColumn(content1,int(col1),header)
+        dico1, h1 = createDicoColumn(content1,int(col1),header1,filetype1,splitCar1)
         #print n_clicks, header, col1, col2
-        dico2, h2 = createDicoColumn(content2,int(col2),header)
+        dico2, h2 = createDicoColumn(content2,int(col2),header2,filetype2,splitCar2)
         #print h1, h2
         final_common_res, final_unique_res = mergeDico(dico1,dico2,h1,h2)
         #return html.A("Download common", href=outname)#)#,html.A("Download unique", href=outname2)
@@ -155,15 +237,15 @@ def getRes(n_clicks,header_val, filename1,content1,col1, filename2,content2,col2
 
 
 
-def treatFile(contents):
+def treatFile(contents,separator):
     #decode content 
     content_type, content_string = contents.split(',')
     #print(contents)
     decoded = (base64.b64decode(content_string)).decode('utf-8')
     #print((decoded.decode('utf-8')))
-    #list_lines = decoded.split("\n")
+
     list_lines = decoded.split("\n")
-    list_header = list_lines[0].split("\t")
+    list_header = list_lines[0].split(separator)
     list_header_tab = []
     '''
     #Create tab formated part = list of dcc.Tab : 
@@ -177,26 +259,38 @@ def treatFile(contents):
     return list_header_tab
 
 
-def createDicoColumn(contents,column_index, no_header):
-    dico = {}
-    content_type, content_string = contents.split(',')
-    decoded = (base64.b64decode(content_string)).decode('utf-8')
+def createDicoColumn(contents,column_index, no_header,separator,splitCar):
+	dico = {}
+	content_type, content_string = contents.split(',')
+	decoded = (base64.b64decode(content_string)).decode('utf-8')
 
-    list_lines = decoded.split("\n")
+	list_lines = decoded.split("\n")
 
-    #if there is no header, all lines are read, starting from line number 0
-    if no_header:
-        h = None
-        line_start = 0
-    #if there is a header, first line is stored in variable h, and we start reading line number 1
-    else:
-        h = list_lines[0]
-        line_start = 1
+	#if there is no header, all lines are read, starting from line number 0
+	if no_header:
+		h = None
+		line_start = 0
+	#if there is a header, first line is stored in variable h, and we start reading line number 1
+	else:
+		h = list_lines[0]
+		line_start = 1
 
-    for line in list_lines[line_start:]:
-        id_line = line.split("\t")[column_index]
-        dico[id_line] = line
-    return dico, h
+    #if we have a csv file, replace "," by tabulations
+	if separator == ",":
+		for line in list_lines[line_start:]:
+			id_line = line.split(separator)[column_index]
+			#remove part of the id if needed
+			if splitCar is not None:
+				id_line = id_line.split(splitCar)[0]
+			dico[id_line] = line.replace(",","\t")
+	else:
+		for line in list_lines[line_start:]:
+			id_line = line.split(separator)[column_index]
+			#remove part of the id if needed
+			if splitCar is not None:
+				id_line = id_line.split(splitCar)[0]
+			dico[id_line] = line
+	return dico, h
 
 
 def mergeDico(dico1,dico2,h1,h2):
@@ -205,10 +299,12 @@ def mergeDico(dico1,dico2,h1,h2):
     unique_res = ""
     if h1 is not None:
         #print h1
-        header_common = h1+"\t"+h2+"\n"
-        common_res+= header_common
         header_unique = h1+"\n"
         unique_res+= header_unique
+        #the header is only written if it is present in both files
+        if h2 is not None:
+        	header_common = h1+"\t"+h2+"\n"
+        	common_res+= header_common
 
     for d in dico1:
         #print d
